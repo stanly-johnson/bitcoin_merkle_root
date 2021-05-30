5de4ae0623c15d8dbe67750f589800cdc85f71eb
Stanly Johnson <stanlyjohnson@outlook.com>
2021-05-30 17:36:02 +0530

    update js to fetch from api

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ The bitcoin merkle tree differs from a standard merkle tree implementation in tw
 1. Ensure you have python3 installed
 2. Open `merkle.py` and change `SELECTED_BLOCK_HASH` to the block hash you want to test
 3. Run `python3 merkle.py` and compare merkle root with block explorer
+
+##### JS Example
+1. Run `npm i`
+2. Open `merkle.js` and change `SELECTED_BLOCK_HASH` to the block hash you want to test
+3. Run `node merkle.js` and compare merkle root with block explorer
--- merkle.js
+++ merkle.js
@@ -1,6 +1,13 @@
 const crypto = require('crypto');
 var reverse = require("buffer-reverse")
-const tx_list = require('./319957.json')
+const got = require('got');
+
+const BASE_URL = "https://api.blockcypher.com/v1/btc/main/blocks/"
+const MAX_TXN = 500
+
+// ------- CHANGE THIS VALUE TO THE BLOCK YOU WANT TO TEST ------- 
+const SELECTED_BLOCK_HASH = "000000000000000018a9a6c39806292529a401918ec55e078306b35884814b7c"
+// ------------------------------------------------------------- 
 
 get_merkle_root = tx_list => {
     
@@ -30,4 +37,29 @@ double_hash = (a,b) => {
     return reverse(Buffer.from(hash2,'utf-8')).toString('hex')
 }
 
-console.log(get_merkle_root(tx_list['txs']))
+get_tx_list = async (block_hash, tx_start = 0, tx_stop = 500) => {
+    let transactions = [];
+    while(true) {
+        let url = BASE_URL + block_hash + "?txstart=" + String(tx_start) + "&limit=" + String(tx_stop);
+        const response = await got(url);
+        let current_txns = JSON.parse(response.body).txids;
+        if(current_txns.length > 0){
+            transactions = transactions.concat(current_txns);
+            tx_start += MAX_TXN;
+            tx_stop += MAX_TXN;
+        }
+        else {
+            break;
+        }
+    }
+    return transactions;
+}
+
+main = async() => {
+    let tx_list = await get_tx_list(SELECTED_BLOCK_HASH);
+    let merkle_root = get_merkle_root(tx_list);
+    console.log(tx_list);
+    console.log(merkle_root);
+}
+
+main();
